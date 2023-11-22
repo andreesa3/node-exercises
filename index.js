de289e0c084a38afba776eb34bef0fe741877560
Andrea <andrea.saitta.as@gmail.com>
2023-11-22 22:25:54 +0100

    ex10

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const fs = require('fs/promises');
-
 const players = ['Joe', 'Caroline', 'Sabrina'];
 
 const luckyDraw = (player) => {
   return new Promise((resolve, reject) => {
     const win = Boolean(Math.round(Math.random()));
-
     if (win) {
-      resolve(`${player} won!`);
+      resolve(`${player} won`);
     } else {
-      reject(`${player} lost!`);
+      reject(`${player} lost`);
     }
-  })
-}
+  });
+};
 
-players.forEach((player) => {
-  luckyDraw(player)
-    .then(res => console.log(res))
-    .catch(err => console.error(err))
-});
+const getResults = async () => {
+  for (const player of players) {
+    try {
+      const result = await luckyDraw(player);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+getResults();
