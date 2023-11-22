9510d5ea10beb6015d23048a72d972e8dec0dd38
Andrea <andrea.saitta.as@gmail.com>
2023-11-22 18:13:20 +0100

    ex08

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 
-const data = 'Contenuto del file di testo 1';
+const data1 = 'Contenuto del file di testo 1';
 
-fs.writeFile('./text1.txt', data, (err) => {
+fs.writeFile('./text1.txt', data1, (err) => {
   if (err) {
     console.error(err);
   }
-  console.log('<Messaggio scritto>');
+  console.log('<Messaggio file 1 scritto>');
+
+
+  const data2 = 'Contenuto del file di testo 2';
+  
+  fs.writeFile('./text2.txt', data2, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log('<Messaggio file 2 scritto>');
+  })
 })
