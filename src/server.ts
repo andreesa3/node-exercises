6361c54c2cfc9d060b325a5886c35d9564157624
Andrea <andrea.saitta.as@gmail.com>
2023-11-28 19:57:13 +0100

    da aggiustare

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} from "./controllers/planets";
 import "express-async-errors";
 
 const app = express();
@@ -8,52 +15,18 @@ const port = process.env.PORT || 3000;
 
 dotenv.config();
 
-type Planet = {
-  id: number;
-  name: string;
-};
-
-type Planets = Planet[];
-
-let planets: Planets = [
-  { id: 1, name: "Mercury" },
-  { id: 2, name: "Venus" },
-  { id: 3, name: "Earth" },
-  { id: 4, name: "Mars" },
-  { id: 5, name: "Jupiter" },
-  { id: 6, name: "Saturn" },
-  { id: 7, name: "Uranus" },
-];
-
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/api/planets", (_, res) => {
-  res.status(200).json(planets);
-});
+app.get("/api/planets", getAll);
 
-app.get("/api/planets/:id", (req, res) => {
-  const { id } = req.params;
-  const planet = planets.find((p) => p.id === Number(id));
-  res.status(200).json(planet);
-});
+app.get("/api/planets/:id", getOneById);
 
-app.post("/api/planets", (req, res) => {
-  console.log(req.body);
-  const id = req.body.id;
-  const name = req.body.name;
+app.post("/api/planets", create);
 
-  if (!id || !name) {
-    return res.status(400).json({ error: "Both 'id' and 'name' are required." });
-  }
+app.put("/api/planets/:id", updateById);
 
-  const newPlanet = { id, name };
-  planets = [...planets, newPlanet];
-
-  console.log(planets);
-
-  res.status(201).json({ msg: "Planet created" });
-});
+app.delete("/api/planets/:id", deleteById);
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
